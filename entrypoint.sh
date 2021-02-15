b8062d3afec266df7206bd68eb26c83388fd00fb
Daniel Pogodda <daniel.pogodda@sportfive.com>
2021-02-15 10:28:04 +0100

    add env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # make sure the arma 3 server directory exists
-mkdir -p ${ARMA3SERVER_DIR} || true
+mkdir -p ${ENV_ARMA3SERVER_DIR} || true
 
 # jump into the arma 3 server directory
-cd ${ARMA3SERVER_DIR}
+cd ${ENV_ARMA3SERVER_DIR}
 
 # make sure the arma 3 server is up-to-date
 steamcmd \
-    +login ${STEAM_ACCOUNT_USER} ${STEAM_ACCOUNT_PASSWORD} \
-    +force_install_dir ${ARMA3SERVER_DIR} \
-    +app_update ${ARMA3SERVER_APP_ID} validate \
+    +login ${ENV_STEAM_ACCOUNT_USER} ${ENV_STEAM_ACCOUNT_PASSWORD} \
+    +force_install_dir ${ENV_ARMA3SERVER_DIR} \
+    +app_update ${ENV_ARMA3SERVER_APP_ID} validate \
     +quit
 
 # start the arma 3 server
-./${ARMA3SERVER_BINARY} \
+./${ENV_ARMA3SERVER_BINARY} \
     -port="2302" \
     -config="configs/server.cfg" \
     -cfg="configs/basic.cfg" \
     -name="default" \
     -world="empty" \
     -profiles="profiles" \
-    -mod={${ARMA3SERVER_PARAMETER_MOD}} \
-    -serverMod={${ARMA3SERVER_PARAMETER_SERVERMOD}} \
+    -mod={${ENV_ARMA3SERVER_PARAMETER_MOD}} \
+    -serverMod={${ENV_ARMA3SERVER_PARAMETER_SERVERMOD}} \
     -loadMissionToMemory
